b1e72ca6b70aa7731962713dd89f1e2c8f59fe15
muannna <evagoeshard@gmail.com>
2022-03-19 22:33:49 +0500

    feat: add adaptive to 748px

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const md = document.querySelectorAll('.modal')
 
 pr.forEach((el, index) => {
     el.addEventListener('mouseover', () => {
-        //md[index].classList.remove('unvisible')
         md[index].classList.add('visible')
     })
 })
 
 pr.forEach((el, index) => {
     el.addEventListener('mouseout', () => {
-        //md[index].classList.add('unvisible')
         md[index].classList.remove('visible')
     })
 })
